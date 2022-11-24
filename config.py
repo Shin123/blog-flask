ecaf649f29606c5f843ba610297127fc67c6582c
shin <vanvietkhtn@gmail.com>
2022-11-24 18:14:36 +0700

    apply factory pattern

--- config.py
+++ config.py
@@ -5,7 +5,8 @@ basedir = os.path.abspath(os.path.dirname(__file__))
 
 class Config(object):
     SQLALCHEMY_TRACK_MODIFICATIONS = False
-    POSTS_PER_PAGE = 5
+    POSTS_PER_PAGE = 10
+    SECRET_KEY = '0\xbb5\x1f\xbe0\xfe\nS\xdaeO\x8c\xc6\xf6T.`/\xb9\xea\x0f*d'
 
 
 class ProdConfig(Config):
@@ -15,9 +16,4 @@ class ProdConfig(Config):
 class DevConfig(Config):
     DEBUG = True
     SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(basedir, 'dev.db')
-    SECRET_KEY = '0\xbb5\x1f\xbe0\xfe\nS\xdaeO\x8c\xc6\xf6T.`/\xb9\xea\x0f*d'
     SQLALCHEMY_TRACK_MODIFICATIONS = True
-
-    # SQLALCHEMY_DATABASE_URI = (
-    #     'postgresql://postgres:postgres@localhost:5432/mastering_flask'
-    # )
